c795bfc1f279dff7985cdbed0570b86c3626f4f7
nverenin <nverenin@gmail.com>
2018-09-18 12:06:14 -0700

    missing the protobuf unit test. it's back in place now.

--- tests/test_suite.cpp
+++ tests/test_suite.cpp
@@ -109,3 +109,26 @@ BOOST_AUTO_TEST_CASE(fs_test_line_reader) {
     lg->info("read {} lines",num);
     BOOST_TEST(num == 34);
 }
+
+BOOST_AUTO_TEST_CASE(protobuf_example_test) {
+    GOOGLE_PROTOBUF_VERIFY_VERSION;
+    lg->info("Starting tests");
+
+    example::Url proto_url;
+    proto_url.set_url("https://www.google.com/");
+    proto_url.set_parse_url(true);
+    proto_url.set_lookup_dns(true);
+
+    std::string data;
+    proto_url.SerializeToString(&data);
+
+    lg->info("created serialized data of size {}",data.size());
+    lg->info("HEXDUMP:\n{}",libnj::util::hexdump(data,16));
+
+    example::Url parsed_url;
+    parsed_url.ParseFromString(data);
+
+    BOOST_TEST((parsed_url.url()) == "https://www.google.com/");
+    BOOST_TEST((parsed_url.parse_url()) == true);
+    BOOST_TEST((parsed_url.lookup_dns()) == true);
+}
